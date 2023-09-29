67c6a1c1dcd0ed35835839dce104b7ff4be85bbe
Arish Imam <62866346+arishimam@users.noreply.github.com>
2023-09-29 13:52:12 -0700

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ let operation = "";
 /* Numbers */
 let numbers = document.getElementsByClassName("number");
 
+const MAX_DIGITS = 13;
+
 for (let num of numbers) {
   num.addEventListener("click", function () {
     let currentDisplay = display.textContent;
@@ -28,8 +30,10 @@ for (let num of numbers) {
 let operators = document.getElementsByClassName("operator");
 
 for (let op of operators) {
+  // op.style.background = "rgb(64, 64, 73)";
+  // operators.style.background = "rgb(64, 64, 73)";
   op.addEventListener("click", function () {
-    if (operation == "") {
+    if (operation == "" && currentNum != "") {
       previousNum = currentNum;
       currentNum = "";
       operation = op.value;
@@ -37,6 +41,7 @@ for (let op of operators) {
       console.log("previousNum = " + previousNum);
       console.log("operation = " + operation);
       console.log("currentNum = " + currentNum);
+      // op.style.background = "#ffac09";
     }
   });
 }
@@ -44,20 +49,34 @@ for (let op of operators) {
 /* Equals */
 document.getElementById("equals").addEventListener("click", function () {
   //   alert("Equals button was clicked!");
-  if (operation == "+") {
-    currentNum = parseFloat(previousNum) + parseFloat(currentNum);
+  // Prevents NaN ?
+  if (previousNum != "" && currentNum != "") {
+    if (operation == "+") {
+      currentNum = parseFloat(previousNum) + parseFloat(currentNum);
+    }
+    if (operation == "-") {
+      currentNum = parseFloat(previousNum) - parseFloat(currentNum);
+    }
+    if (operation == "*") {
+      currentNum = parseFloat(previousNum) * parseFloat(currentNum);
+    }
+    if (operation == "/") {
+      currentNum = parseFloat(previousNum) / parseFloat(currentNum);
+    }
+    operation = "";
+
+    // Checks number length and rounds if necessary. Incomplete
+    let stringNum = currentNum.toString();
+    if (stringNum.length > MAX_DIGITS) {
+      if (stringNum.includes(".")) {
+        let decimalPlaces = MAX_DIGITS - (stringNum.indexOf(".") + 1);
+        currentNum = parseFloat(currentNum).toFixed(decimalPlaces);
+      } else {
+        currentNum = parseFloat(currentNum).toExponential();
+      }
+    }
+    display.textContent = currentNum;
   }
-  if (operation == "-") {
-    currentNum = parseFloat(previousNum) - parseFloat(currentNum);
-  }
-  if (operation == "*") {
-    currentNum = parseFloat(previousNum) * parseFloat(currentNum);
-  }
-  if (operation == "/") {
-    currentNum = parseFloat(previousNum) / parseFloat(currentNum);
-  }
-  operation = "";
-  display.textContent = currentNum;
 });
 
 /* Clear Display and Memory*/
